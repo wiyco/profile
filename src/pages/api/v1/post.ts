ad90ae2e9a2b7d2ada3d57ad09f518913c3fd8c6
wiyco <72733890+wiyco@users.noreply.github.com>
2023-05-09 11:31:06 +0900

    Set `runtime: edge` for deploy to `Cloudflare pages`

--- src/pages/api/v1/post.ts
+++ src/pages/api/v1/post.ts
@@ -1,6 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { getPost } from "@/hooks/getSupabase";
 
+export const config = { runtime: 'edge' };
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const slug = req.query.s ?? "1";
   const post = await getPost(`${slug}`);
--- src/pages/api/v1/posts.ts
+++ src/pages/api/v1/posts.ts
@@ -1,6 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { getPosts } from "@/hooks/getSupabase";
 
+export const config = { runtime: 'edge' };
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const posts = await getPosts();
   res.status(200).json({ posts });
