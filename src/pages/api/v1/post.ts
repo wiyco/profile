a52179199b33160751f74049e806f83944dc4dc9
wiyco <72733890+wiyco@users.noreply.github.com>
2023-05-12 14:14:00 +0900

    Fix an error caused by `/v1/posts` api

--- src/pages/api/v1/post.ts
+++ src/pages/api/v1/post.ts
@@ -4,7 +4,7 @@ import { getPost } from "@/hooks/getSupabase";
 export const config = { runtime: "edge" };
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const slug = req.query.s ?? "1";
+  const slug = req.query.s! ?? "1";
   const post = await getPost(`${slug}`);
   res.status(200).json({ post });
 }
