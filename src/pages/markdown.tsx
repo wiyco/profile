bfe743991d01c35829f817785f3a9b743a22f06d
wiyco <72733890+wiyco@users.noreply.github.com>
2023-05-20 12:01:13 +0900

    Update markdown styles

--- src/pages/markdown.tsx
+++ src/pages/markdown.tsx
@@ -4,27 +4,29 @@ import MarkdownRenderer from "@/components/renderers/MarkdownRenderer";
 export default function Markdown() {
   const data = {
     title: "Markdown/Demo",
-    body: `# Header# (h2)\n\n## Header## (h3)\n\n### Header### (h4)\n\n#### Header#### (h5)
-    \n\nThis is text
-    \n\nThis is horizontal separator
-    \n\n---
-    \n\n[This is an external link](https://github.com/wiyco/profile)
-    \n\n[This is an internal link](/blog/test)
-    \n\n**This is a strong text**
-    \n\n> This is a quote text
-    \n\n![This is an image](https://www.dropbox.com/s/ectb0kjn8gcnu8j/profile-supabase-erd.jpg?raw=1)
-    \n\n- list 0
-    \n- list 1
-    \n\n\`\`\`This is a code-text between 3 backtick (0,1)\`\`\`
-    \n\n\`\`\`python
-    \ndef code_block():
-    \n  print("This is a code-block (0,1)")
-    \n  while True:
-    \n    pass
-    \n\`\`\`
-    \n\n<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/ZRtdQ81jPUQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-    \n\nDEMO END
-    `,
+    body: `# Header# (h2)
+\n## Header## (h3)
+\n### Header### (h4)
+\n#### Header#### (h5)
+\nThis is text
+\nThe below line is horizontal separator
+\n---
+\n[This is an external link](https://github.com/wiyco/profile)
+\n[This is an internal link](/blog/test)
+\n**This is a strong text**
+\n> This is a quote text
+\n- list 0
+\n- list 1
+\n\`\`\`This is a code-text between 3 backtick (0,1)\`\`\`
+\n\`\`\`python
+def code_block():
+  print("This is a code-block (0,1)")
+  while True:
+    pass
+\n\`\`\`
+\n![This is an image](https://www.dropbox.com/s/ectb0kjn8gcnu8j/profile-supabase-erd.jpg?raw=1)
+\n<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/ZRtdQ81jPUQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+\nDEMO END`,
   };
 
   return (
