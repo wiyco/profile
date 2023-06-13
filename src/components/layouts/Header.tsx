8a6a155ff2ccbbb273b0d994a955e720a070d965
wiyco <72733890+wiyco@users.noreply.github.com>
2023-06-13 11:48:35 +0900

    Redesign shadow (#8)
    * Redesign `Home` page
    
    * Redesign shadow

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -7,7 +7,7 @@ const raleway = Raleway({ subsets: ["latin"] });
 
 export default function Header() {
   return (
-    <header className="z-50 sticky top-0 flex items-center justify-center w-full p-4 drop-shadow backdrop-blur">
+    <header className="z-50 sticky top-0 flex items-center justify-center w-full p-4 shadow backdrop-blur">
       <div className="w-full max-w-5xl flex items-center justify-between">
         <span className={`font-mono text-2xl ${raleway.className}`}>
           <Link href={"/"}>wiyco</Link>
