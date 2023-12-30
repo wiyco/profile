75e7796737bec14ae3485e59019d7fc82e8c3d25
wiyco <72733890+wiyco@users.noreply.github.com>
2023-12-30 16:05:33 +0900

    [Metadata] Define detailed metadata for all pages (#25)

--- src/app/(pages)/archive/layout.tsx
+++ src/app/(pages)/archive/layout.tsx
@@ -6,6 +6,36 @@ export const metadata: Metadata = {
     template: "%s - Archive | wiyco",
   },
   description: "wiyco's archive.",
+  openGraph: {
+    title: "Archive | wiyco",
+    description: "wiyco's archive.",
+    type: "website",
+    url: "/archive",
+    siteName: "wiyco",
+    images: [
+      {
+        type: "image/png",
+        url: "/android-chrome-512x512.png?v=0.1",
+        width: 512,
+        height: 512,
+        alt: "wiyco",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary",
+    title: "Archive | wiyco",
+    description: "wiyco's archive.",
+    images: [
+      {
+        type: "image/png",
+        url: "/android-chrome-512x512.png?v=0.1",
+        width: 512,
+        height: 512,
+        alt: "wiyco",
+      },
+    ],
+  },
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
