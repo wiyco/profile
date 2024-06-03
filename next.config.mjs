95c5c1df6f63f2a708d57e4d8529740502abc0ad
wiyco <me@mail.wiyco.dev>
2024-06-04 03:14:02 +0900

    chore: next config due to latest packages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
-/** @type {import('next').NextConfig} */
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+/** @type {import("next").NextConfig} */
 const nextConfig = {
+  /** @see {@link https://nextjs.org/docs/app/building-your-application/styling/sass} */
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
   images: {
+    /** @see {@link https://nextjs.org/docs/app/api-reference/components/image#minimumcachettl} */
     minimumCacheTTL: 14400, // 4 hours
     remotePatterns: [
       /** @see {@link https://emojiapi.dev} */
@@ -35,4 +46,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
