90792faac472ff2dec60188d6313c2448ddb2a9d
wiyco <me@mail.wiyco.dev>
2024-06-19 05:35:01 +0900

    refactor: catch all fetching errors

--- src/utils/fetcher/post.ts
+++ src/utils/fetcher/post.ts
@@ -5,24 +5,38 @@ async function getPosts(
   limit: PaginationApiRequest["limit"],
   offset: PaginationApiRequest["offset"]
 ): Promise<PaginationApiResponce<PostWithRelations>> {
-  return await fetch(
-    `${process.env.NEXT_PUBLIC_ORIGIN_URL}/api/v0/posts?limit=${limit}&offset=${offset}`,
-    { next: { revalidate: 3600 } }
-  )
-    .then((res) => res.json())
-    .catch((e) => {
-      console.error(e);
-    });
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_ORIGIN_URL}/api/v0/posts?limit=${limit}&offset=${offset}`,
+      { next: { revalidate: 3600 } }
+    );
+    if (!res.ok) {
+      console.error(res.statusText);
+      return { results: [], count: 0 };
+    }
+    const data: PaginationApiResponce<PostWithRelations> = await res.json();
+    return data;
+  } catch (e) {
+    console.error(e);
+    return { results: [], count: 0 };
+  }
 }
 
-async function getPost(id: PostWithRelations["id"]): Promise<PostWithRelations> {
-  return await fetch(`${process.env.NEXT_PUBLIC_ORIGIN_URL}/api/v0/post/${id}`, {
-    next: { revalidate: 3600 },
-  })
-    .then((res) => res.json())
-    .catch((e) => {
-      console.error(e);
+async function getPost(id: PostWithRelations["id"]): Promise<PostWithRelations | null> {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_ORIGIN_URL}/api/v0/post/${id}`, {
+      next: { revalidate: 3600 },
     });
+    if (!res.ok) {
+      console.error(res.statusText);
+      return null;
+    }
+    const data: PostWithRelations = await res.json();
+    return data;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
 }
 
 export { getPost, getPosts };
