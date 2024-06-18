5a625db4bb164d154496f256df8fb4918945c9df
wiyco <me@mail.wiyco.dev>
2024-06-19 05:34:33 +0900

    refactor: separate components (header)

--- src/components/buttons/MenuButton.tsx
+++ src/components/buttons/MenuButton.tsx
@@ -1,26 +1,68 @@
 "use client";
 
+import Check from "@icons/check.svg";
 import Menu from "@icons/menu.svg";
 import Xmark from "@icons/xmark.svg";
+import { Button, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from "@nextui-org/react";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
+
+import { cn } from "@/utils/cn";
+import { isCurrentPath } from "@/utils/path";
 
 type MenuButtonProps = {
-  className?: React.HTMLAttributes<HTMLButtonElement>["className"];
-  openState: {
-    setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
-    isOpen: boolean;
-  };
+  items?: {
+    label: string;
+    icon: React.ReactNode;
+    url: string;
+  }[];
 };
 
-export function MenuButton({ className, openState }: MenuButtonProps) {
+export function MenuButton({ items = [] }: MenuButtonProps) {
+  const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <button
-      className={className}
-      type="button"
-      title="Menu"
-      aria-label="Toggle Menu"
-      onClick={() => openState.setIsOpen((prev) => !prev)}
-    >
-      <span className="stroke-current">{openState.isOpen ? <Xmark /> : <Menu />}</span>
-    </button>
+    <Dropdown backdrop="blur" onOpenChange={(isOpen) => setIsOpen(isOpen)}>
+      <DropdownTrigger>
+        <Button
+          disableRipple
+          className="h-6 min-w-6 bg-transparent p-0 data-[hover=true]:bg-transparent"
+          variant="light"
+          radius="sm"
+        >
+          <span
+            className={cn(
+              "stroke-current transition-transform duration-300 ease-in-out",
+              isOpen ? "rotate-180" : "rotate-0"
+            )}
+          >
+            {isOpen ? <Xmark /> : <Menu />}
+          </span>
+        </Button>
+      </DropdownTrigger>
+      <DropdownMenu aria-label="Navigation">
+        {items.map((item) => (
+          <DropdownItem
+            key={`head-nav-${item.label}`}
+            href={item.url}
+            startContent={<span className="stroke-current">{item.icon}</span>}
+            endContent={
+              isCurrentPath(pathname, item.url) && (
+                <span className="stroke-current text-tiny">
+                  <Check />
+                </span>
+              )
+            }
+            className={cn(
+              "py-2.5",
+              isCurrentPath(pathname, item.url) ? "opacity-100" : "opacity-60"
+            )}
+          >
+            {item.label.charAt(0).toUpperCase() + item.label.slice(1)}
+          </DropdownItem>
+        ))}
+      </DropdownMenu>
+    </Dropdown>
   );
 }
