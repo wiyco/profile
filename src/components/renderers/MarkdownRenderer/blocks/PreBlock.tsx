12783293e0bc99f7ea22326f59a241e8a4c841d3
wiyco <me@mail.wiyco.dev>
2024-06-19 21:42:41 +0900

    feat: restyle margin due to fonts

--- src/components/renderers/MarkdownRenderer/blocks/PreBlock.tsx
+++ src/components/renderers/MarkdownRenderer/blocks/PreBlock.tsx
@@ -6,8 +6,8 @@ export function PreBlock(props: React.ComponentProps<"pre">) {
       <Snippet
         hideSymbol
         classNames={{
-          base: "mb-6 mt-1 pl-5 pr-11 w-full overflow-x-auto leading-[1.375rem]",
-          copyButton: "mb-6 mt-1 absolute right-2 top-[7px] backdrop-blur-xl",
+          base: "mb-8 pl-5 pr-11 w-full overflow-x-auto leading-[1.375rem]",
+          copyButton: "mb-8 absolute right-2 top-[7px] backdrop-blur-xl",
           pre: "py-1.5 whitespace-pre",
         }}
       >
