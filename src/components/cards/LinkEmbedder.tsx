066597a5618163aa1f77a746165660813a64f4da
wiyco <me@mail.wiyco.dev>
2024-06-23 06:17:47 +0900

    hotfix: text area width on Safari

--- src/components/cards/LinkEmbedder.tsx
+++ src/components/cards/LinkEmbedder.tsx
@@ -43,45 +43,43 @@ export async function LinkEmbedder({ href, className }: LinkEmbedderProps) {
         className
       )}
     >
-      <Card shadow="none" radius="none" className="bg-inherit">
-        <CardBody className="h-36 flex-row p-0">
-          <div className="flex items-center justify-start">
-            <div className="relative aspect-square h-full w-fit md:aspect-[40/21]">
+      <Card shadow="none" radius="none" className="w-full bg-inherit">
+        <CardBody className="flex h-36 w-full flex-row items-center justify-start p-0">
+          <div className="relative aspect-square h-full w-fit md:aspect-[40/21]">
+            <Image
+              as={NextImage}
+              radius="none"
+              classNames={{ wrapper: "h-full w-full !max-w-none" }}
+              className="h-full w-full object-cover"
+              src={image}
+              alt={title || "No Image"}
+              sizes="(max-width: 768px) 50vw, 50vw"
+              fill
+              priority
+              unoptimized
+            />
+          </div>
+          <section className="grid h-full w-full place-content-center gap-3 px-6 py-3.5">
+            <span className="line-clamp-3 text-sm font-medium md:text-base lg:line-clamp-2">
+              {title || domain}
+            </span>
+            <span className="hidden text-xs font-light text-neutral-800/80 dark:text-neutral-200/80 md:line-clamp-2">
+              {description}
+            </span>
+            <small className="flex items-center space-x-1.5">
               <Image
                 as={NextImage}
                 radius="none"
-                classNames={{ wrapper: "h-full w-full !max-w-none" }}
-                className="h-full w-full object-cover"
-                src={image}
-                alt={title || "No Image"}
-                sizes="(max-width: 768px) 50vw, 50vw"
-                fill
-                priority
+                src={favicon}
+                alt={domain}
+                width={16}
+                height={16}
+                sizes="16px"
                 unoptimized
               />
-            </div>
-            <section className="grid h-full w-full place-content-center gap-3 px-6 py-3.5">
-              <span className="line-clamp-3 text-sm font-medium md:text-base lg:line-clamp-2">
-                {title || domain}
-              </span>
-              <span className="hidden text-xs font-light text-neutral-800/80 dark:text-neutral-200/80 md:line-clamp-2">
-                {description}
-              </span>
-              <small className="flex items-center space-x-1.5">
-                <Image
-                  as={NextImage}
-                  radius="none"
-                  src={favicon}
-                  alt={domain}
-                  width={16}
-                  height={16}
-                  sizes="16px"
-                  unoptimized
-                />
-                <span className="line-clamp-1 text-xs font-light opacity-90">{domain}</span>
-              </small>
-            </section>
-          </div>
+              <span className="line-clamp-1 text-xs font-light opacity-90">{domain}</span>
+            </small>
+          </section>
         </CardBody>
       </Card>
     </Link>
