a4d7577f43764dc929d4f709cfa251d3a525c716
wiyco <me@mail.wiyco.dev>
2024-06-23 03:38:25 +0900

    feat: unwrap link embed card from `p` tag

--- src/components/renderers/MarkdownRenderer/index.tsx
+++ src/components/renderers/MarkdownRenderer/index.tsx
@@ -24,6 +24,30 @@ export function MarkdownRenderer({ children }: { children: string }) {
         h4: "h5",
         h5: "h6",
         h6: "h6",
+        p: (props) => {
+          /** Unwrap the paragraph */
+          if (props.node?.children.length === 1) {
+            const child = props.node?.children[0];
+            /**
+             * If there's only one child and it's a `a` tag and the text is the same as the href,
+             * return the unwrapped paragraph to render the link as a block element.
+             * @example
+             * https://github.com
+             * @see LinkBlock -> LinkEmbedder
+             */
+            if (child.type === "element" && child.tagName === "a") {
+              if (
+                child.children[0].type === "element" &&
+                child.children[0].children[0].type === "text" &&
+                child.children[0].children[0].value === props.content
+              ) {
+                return <>{props.children}</>;
+              }
+            }
+          }
+          /** Wrap the paragraph with a `p` tag */
+          return <p>{props.children}</p>;
+        },
         a: (props) => LinkBlock(props),
         img: (props) => ImageBlock(props),
         iframe: (props) => IframeBlock(props),
