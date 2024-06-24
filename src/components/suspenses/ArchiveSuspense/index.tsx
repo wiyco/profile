9a6096eac3a9643fff66dfa19f83214e4a5f4cb0
wiyco <me@mail.wiyco.dev>
2024-06-24 23:24:11 +0900

    refactor: correct archive suspense classNames

--- src/components/suspenses/ArchiveSuspense/index.tsx
+++ src/components/suspenses/ArchiveSuspense/index.tsx
@@ -8,7 +8,7 @@ export function ArchiveSuspense() {
     <section className="grid grid-cols-1">
       <section className="flex md:grid md:grid-cols-6">
         <aside className="timeline pl-2 md:col-span-1 md:pr-6"></aside>
-        <CardFullSkeleton className="flex-1 py-4 pl-6 md:col-span-5 md:pl-8" />
+        <CardFullSkeleton className="flex-1 py-4 pl-6 md:col-span-5 md:flex-none md:pl-8" />
       </section>
       <section className="flex md:grid md:grid-cols-6">
         <aside className="timeline pl-2 md:col-span-1 md:pr-6"></aside>
@@ -18,13 +18,13 @@ export function ArchiveSuspense() {
       </section>
       <section className="flex md:grid md:grid-cols-6">
         <aside className="timeline pl-2 md:col-span-1 md:pr-6"></aside>
-        <article className="md:w-ful my-4 ml-6 w-full md:col-span-5 md:w-auto">
+        <article className="my-4 ml-6 w-full md:col-span-5 md:ml-8 md:w-auto">
           <LinkEmbedderSkeleton className="timeline-item-animation" />
         </article>
       </section>
       <section className="flex md:grid md:grid-cols-6">
         <aside className="timeline pl-2 md:col-span-1 md:pr-6"></aside>
-        <CardFullSkeleton className="flex-1 py-4 pl-6 md:col-span-5 md:pl-8" />
+        <CardFullSkeleton className="flex-1 py-4 pl-6 md:col-span-5 md:flex-none md:pl-8" />
       </section>
     </section>
   );
