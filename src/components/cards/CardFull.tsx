49a58f1fa6db5ee80528a07015914106519ae0f6
wiyco <me@mail.wiyco.dev>
2024-07-13 01:31:15 +0900

    refactor(archive): unwrap framer-motion

--- src/components/cards/CardFull.tsx
+++ src/components/cards/CardFull.tsx
@@ -1,12 +1,10 @@
-"use client";
-
 import { ImageLinks } from "@constants/links";
-import { Card, CardFooter, Image } from "@nextui-org/react";
-import { motion } from "framer-motion";
+import { Card, CardFooter, Image, Link } from "@nextui-org/react";
 import NextImage from "next/image";
-import Link from "next/link";
+import NextLink from "next/link";
 
 import { cn } from "@/utils/cn";
+import { isExternalPath } from "@/utils/path";
 
 type CardFullProps = {
   title: string;
@@ -14,9 +12,6 @@ type CardFullProps = {
   isOGImage?: boolean;
   url: string;
   isExternal?: boolean;
-  animation?: {
-    delay?: number;
-  };
   className?: string;
 };
 
@@ -26,56 +21,43 @@ export function CardFull({
   isOGImage,
   url,
   isExternal,
-  animation,
   className,
 }: CardFullProps) {
-  const externalOptions = isExternal && {
-    target: "_blank",
-    rel: "noopener noreferrer",
-  };
-
   return (
-    <motion.article
-      className={className}
-      initial={{ y: "2rem", opacity: 0 }}
-      whileInView={{ y: 0, opacity: 1 }}
-      viewport={{ once: true, amount: 0.25 }}
-      transition={{
-        type: "spring",
-        stiffness: 220,
-        damping: 20,
-        bounce: 0,
-        delay: animation?.delay || 0,
-      }}
+    <Link
+      as={NextLink}
+      href={url}
+      isExternal={isExternal}
+      title={title || undefined}
+      className={cn("w-full overflow-clip rounded-xl shadow-md", className)}
     >
-      <Link href={url} style={{ textDecoration: "none" }} {...externalOptions}>
-        <Card
-          shadow="none"
-          isFooterBlurred
-          classNames={{
-            base: "aspect-16/10 w-full overflow-clip border-none shadow-md",
-          }}
-          className="origin-left scale-100 bg-neutral-400/10 !transition-transform duration-250 ease-in-out hover:scale-[1.0125] active:scale-[0.99] dark:bg-neutral-600/10"
-        >
-          <Image
-            as={NextImage}
-            classNames={{ wrapper: "h-full w-full !max-w-none" }}
-            className={cn(
-              "h-full w-full",
-              isOGImage ? "object-contain object-top" : "object-cover object-center"
-            )}
-            src={thumbnail || ImageLinks.FALLBACK}
-            alt={title}
-            sizes="(max-width: 768px) 100vw, 50vw"
-            fill
-            priority
-            unoptimized={isExternal}
-          />
-          <CardFooter className="absolute inset-x-0 bottom-1 z-10 mx-auto grid w-[calc(100%-0.5rem)] place-content-center rounded-medium py-2.5 backdrop-brightness-[.7] md:py-[1.375rem]">
-            <span className="break-words text-center text-small text-white">{title}</span>
-          </CardFooter>
-        </Card>
-      </Link>
-    </motion.article>
+      <Card
+        shadow="none"
+        radius="none"
+        isFooterBlurred
+        classNames={{
+          base: "aspect-16/10 w-full overflow-clip border-none",
+        }}
+        className="bg-neutral-400/10 dark:bg-neutral-600/10"
+      >
+        <Image
+          as={NextImage}
+          classNames={{ wrapper: "h-full w-full !max-w-none" }}
+          className={cn(
+            "h-full w-full",
+            isOGImage ? "object-contain object-top" : "object-cover object-center"
+          )}
+          src={thumbnail || ImageLinks.FALLBACK}
+          alt={title}
+          sizes="(max-width: 768px) 100vw, 50vw"
+          fill
+          priority
+          unoptimized={isExternalPath(thumbnail || ImageLinks.FALLBACK)}
+        />
+        <CardFooter className="absolute inset-x-0 bottom-1 z-10 mx-auto grid w-[calc(100%-0.5rem)] place-content-center rounded-medium py-2.5 backdrop-brightness-[.7] md:py-[1.375rem]">
+          <span className="break-words text-center text-small text-white">{title}</span>
+        </CardFooter>
+      </Card>
+    </Link>
   );
 }
--- src/components/suspenses/ArchiveSuspense/index.tsx
+++ src/components/suspenses/ArchiveSuspense/index.tsx
@@ -8,12 +8,8 @@ export function ArchiveSuspense() {
     <section className="grid grid-cols-1">
       <section className="flex md:grid md:grid-cols-6">
         <aside className="timeline pl-2 md:col-span-1 md:pr-6"></aside>
-        <CardFullSkeleton className="flex-1 py-4 pl-6 md:col-span-5 md:flex-none md:pl-8" />
-      </section>
-      <section className="flex md:grid md:grid-cols-6">
-        <aside className="timeline pl-2 md:col-span-1 md:pr-6"></aside>
-        <article className="my-4 ml-6 w-full md:col-span-5 md:ml-8 md:w-auto">
-          <LinkEmbedderSkeleton className="timeline-item-animation" />
+        <article className="my-4 ml-6 flex-1 md:col-span-5 md:ml-8">
+          <CardFullSkeleton className="timeline-item-animation" />
         </article>
       </section>
       <section className="flex md:grid md:grid-cols-6">
@@ -24,7 +20,15 @@ export function ArchiveSuspense() {
       </section>
       <section className="flex md:grid md:grid-cols-6">
         <aside className="timeline pl-2 md:col-span-1 md:pr-6"></aside>
-        <CardFullSkeleton className="flex-1 py-4 pl-6 md:col-span-5 md:flex-none md:pl-8" />
+        <article className="my-4 ml-6 w-full md:col-span-5 md:ml-8 md:w-auto">
+          <LinkEmbedderSkeleton className="timeline-item-animation" />
+        </article>
+      </section>
+      <section className="flex md:grid md:grid-cols-6">
+        <aside className="timeline pl-2 md:col-span-1 md:pr-6"></aside>
+        <article className="my-4 ml-6 flex-1 md:col-span-5 md:ml-8">
+          <CardFullSkeleton className="timeline-item-animation" />
+        </article>
       </section>
     </section>
   );
