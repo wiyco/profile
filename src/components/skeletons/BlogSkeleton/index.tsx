f35f2f385e57e1258af1a5285559ef06538d8011
wiyco <me@mail.wiyco.dev>
2024-07-14 00:55:39 +0900

    refactor(blog): delete animation class name from skeleton

--- src/components/skeletons/BlogSkeleton/index.tsx
+++ src/components/skeletons/BlogSkeleton/index.tsx
@@ -7,9 +7,9 @@ export function BlogSkeleton() {
   return (
     <>
       <section className="grid grid-cols-1 gap-6 md:grid-cols-3">
-        <CardPostSkeleton className="blog-item-animation" />
-        <CardPostSkeleton className="blog-item-animation" />
-        <CardPostSkeleton className="blog-item-animation" />
+        <CardPostSkeleton />
+        <CardPostSkeleton />
+        <CardPostSkeleton />
       </section>
       <Pagination className="mx-auto" count={3} itemsPerPage={3} initialPage={1} isDisabled />
     </>
