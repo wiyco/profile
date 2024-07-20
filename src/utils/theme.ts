4e58eaeee67ceb4956f25b71de88c7864989c6dd
wiyco <me@mail.wiyco.dev>
2024-07-21 02:14:46 +0900

    refactor(theme): wrapped script with try-catch

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -3,14 +3,18 @@
  * @see RootLayout
  */
 (function () {
-  const prefers = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
-  /** next-themes color theme key */
-  const fromStorage = localStorage.getItem("theme");
-  const colorMode: "light" | "dark" =
-    fromStorage === "system" || fromStorage == null
-      ? prefers
-      : fromStorage === "dark"
-        ? "dark"
-        : "light";
-  window.document.documentElement.dataset.colorMode = colorMode;
+  try {
+    const prefers = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+    /** next-themes color theme key */
+    const fromStorage = localStorage.getItem("theme");
+    const colorMode: "light" | "dark" =
+      fromStorage === "system" || fromStorage == null
+        ? prefers
+        : fromStorage === "dark"
+          ? "dark"
+          : "light";
+    document.documentElement.classList.add(colorMode);
+  } catch (_) {
+    console.warn("Failed to initialize color mode.");
+  }
 })();
