155ddaa5f9f8f7242d329b98d8555a1ba442ef4f
wiyco <me@mail.wiyco.dev>
2024-07-30 02:29:30 +0900

    refactor(card-post): skeletons roundness

--- src/components/cards/CardPost/skeleton.tsx
+++ src/components/cards/CardPost/skeleton.tsx
@@ -18,16 +18,16 @@ export function CardPostSkeleton({ className }: CardPostSkeletonProps) {
       className={cn("bg-neutral-400/10 dark:bg-neutral-600/10", className)}
     >
       <CardHeader>
-        <Skeleton className="h-24 w-full rounded-lg" />
+        <Skeleton className="h-24 w-full rounded-full" />
         <Skeleton classNames={{ base: "absolute right-3 top-3 z-10 w-8 h-8 rounded-full" }} />
       </CardHeader>
       <CardBody>
-        <Skeleton className="h-6 w-full rounded-lg" />
+        <Skeleton className="h-6 w-full rounded-full" />
       </CardBody>
       <CardFooter>
         <div className="grid w-full grid-cols-2 items-end gap-2.5 text-small text-neutral-500 dark:text-neutral-400">
-          <Skeleton className="h-5 w-12 rounded-lg" />
-          <Skeleton className="h-5 w-20 place-self-end rounded-lg" />
+          <Skeleton className="h-5 w-12 rounded-full" />
+          <Skeleton className="h-5 w-20 place-self-end rounded-full" />
         </div>
       </CardFooter>
     </Card>
