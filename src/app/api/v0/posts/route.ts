97dc94a3d03ca1dfa1cca301600ae41c5efe0916
wiyco <me@mail.wiyco.dev>
2024-08-27 00:37:34 +0900

    refactor(api-posts): revalidate 600 to 3600

--- src/app/api/v0/posts/route.ts
+++ src/app/api/v0/posts/route.ts
@@ -6,7 +6,7 @@ import { paginationApiRequest } from "@/types/api/pagination";
 
 /** @see {@link https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config} */
 export const dynamic = "auto";
-export const revalidate = 600;
+export const revalidate = 3600;
 export const maxDuration = 10;
 
 export async function GET(request: NextRequest) {
