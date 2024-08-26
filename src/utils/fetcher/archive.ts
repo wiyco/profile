e935d2ec43be469555445e1868142fafb2619243
wiyco <me@mail.wiyco.dev>
2024-08-27 00:38:27 +0900

    refactor(archive): fetch revalidate 600 to 3600

--- src/utils/fetcher/archive.ts
+++ src/utils/fetcher/archive.ts
@@ -5,7 +5,7 @@ import type { Archive } from "@/types";
 async function getArchives(): Promise<Archive[]> {
   try {
     const res = await fetch(process.env.NEXT_PUBLIC_ARCHIVE_URL!, {
-      next: { revalidate: 600 },
+      next: { revalidate: 3600 },
     });
     if (!res.ok) {
       console.error(res.statusText);
