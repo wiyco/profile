2a87eab7808223ad88107856783f0d36613591af
wiyco <me@mail.wiyco.dev>
2024-09-01 03:17:25 +0900

    refactor(metadata-extractor): allow cors

--- src/utils/embedder/metadata.ts
+++ src/utils/embedder/metadata.ts
@@ -26,7 +26,11 @@ type ExtractedMetadata = {
 async function getMetadataFromUrl(url: string): Promise<ExtractedMetadata | null> {
   try {
     /** @see {@link https://github.com/laurengarcia/url-metadata?tab=readme-ov-file#usage} */
-    const metadata = await urlMetadata(url, { cache: "no-cache", descriptionLength: 200 });
+    const metadata = await urlMetadata(url, {
+      cache: "no-cache",
+      mode: "cors",
+      descriptionLength: 200,
+    });
     return metadata as ExtractedMetadata;
   } catch (e) {
     console.error(e);
