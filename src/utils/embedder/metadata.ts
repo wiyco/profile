8599e7d60e17a0c93cc0b4b5117fbc7db4a0c7d5
wiyco <me@mail.wiyco.dev>
2024-09-01 03:20:21 +0900

    fix(metadata-extractor): fetch failure due to 429 error from YouTube

--- src/utils/embedder/metadata.ts
+++ src/utils/embedder/metadata.ts
@@ -27,7 +27,7 @@ async function getMetadataFromUrl(url: string): Promise<ExtractedMetadata | null
   try {
     /** @see {@link https://github.com/laurengarcia/url-metadata?tab=readme-ov-file#usage} */
     const metadata = await urlMetadata(url, {
-      cache: "no-cache",
+      cache: "force-cache",
       mode: "cors",
       descriptionLength: 200,
     });
