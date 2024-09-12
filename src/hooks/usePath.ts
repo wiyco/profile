526f2f7dd95c450d8dd154a7c44da94f670ff056
wiyco <me@mail.wiyco.dev>
2024-09-12 11:00:39 +0900

    refactor(use-path): remove unused hooks

--- src/hooks/usePath.ts
+++ src/hooks/usePath.ts
@@ -7,19 +7,7 @@ export function usePath() {
   /** @example / */
   const isRoot = useMemo(() => pathname === "/", [pathname]);
 
-  /** @example /archive */
-  const isArchive = useMemo(() => pathname === "/archive", [pathname]);
-
-  /** @example /blog */
-  const isBlog = useMemo(() => pathname === "/blog", [pathname]);
-
-  /** @example /blog/25bepuTC */
-  const isBlogPost = useMemo(() => pathname.match(/\/blog\/[a-zA-Z0-9_\-=]+/) !== null, [pathname]);
-
   return {
     isRoot,
-    isArchive,
-    isBlog,
-    isBlogPost,
   };
 }
