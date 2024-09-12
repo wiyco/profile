fa275df717ea6bff14477f0410b59bcaf30e55f7
wiyco <me@mail.wiyco.dev>
2024-09-12 09:03:22 +0900

    refactor(api): merge import statements

--- src/app/api/v0/posts/route.ts
+++ src/app/api/v0/posts/route.ts
@@ -1,5 +1,4 @@
-import type { NextRequest } from "next/server";
-import { NextResponse } from "next/server";
+import { type NextRequest, NextResponse } from "next/server";
 
 import { prisma } from "@/lib/prisma";
 import { paginationApiRequest } from "@/types/api/pagination";
