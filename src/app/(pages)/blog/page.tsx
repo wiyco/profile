d09a91256cb6fbecaf8f3caa791ff384bf9d92df
wiyco <72733890+wiyco@users.noreply.github.com>
2024-10-09 08:49:57 +0900

    fix(blog): safe param of page /min, max patterns

--- src/app/(pages)/blog/page.tsx
+++ src/app/(pages)/blog/page.tsx
@@ -1,17 +1,18 @@
 import { Suspense } from "react";
 
 import { BlogSkeleton } from "@/components/skeletons/BlogSkeleton";
+import type { SearchParams } from "@/types/next";
 
 import { BlogServer } from "./blog.server";
 
-export default function Page() {
+export default function Page({ searchParams }: { searchParams: SearchParams }) {
   return (
     <div className="default-wrap mx-auto max-w-screen-lg !content-between">
       <h1>
         <span>Blog</span>
       </h1>
       <Suspense fallback={<BlogSkeleton />}>
-        <BlogServer />
+        <BlogServer searchParams={searchParams} />
       </Suspense>
     </div>
   );
