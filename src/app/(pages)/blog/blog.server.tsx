e2c323752f7f62bb56c9128be3c477cb2483e72d
wiyco <72733890+wiyco@users.noreply.github.com>
2024-10-09 09:01:24 +0900

    Revert "fix(blog): safe param of page /min, max patterns"
    This reverts commit d09a91256cb6fbecaf8f3caa791ff384bf9d92df.

--- src/app/(pages)/blog/blog.server.tsx
+++ src/app/(pages)/blog/blog.server.tsx
@@ -1,28 +1,10 @@
-import { redirect } from "next/navigation";
-
-import type { SearchParams } from "@/types/next";
 import { getPosts } from "@/utils/fetcher/post";
 
 import { Blog } from "./blog.client";
 import { paginationSettings } from "./constants";
 
-type BlogServerProps = {
-  searchParams: SearchParams;
-};
-
-export async function BlogServer({ searchParams }: BlogServerProps) {
-  const pageNumber = Number(searchParams.page);
-  /** No isNaN() due to leave the URL when page parameter isn't set */
-  if (pageNumber < 1) {
-    redirect(`?page=1`);
-  }
-
+export async function BlogServer() {
   const data = await getPosts(paginationSettings.itemsPerPage, 0);
-
-  const totalPageNumber = Math.ceil(data.count / paginationSettings.itemsPerPage);
-  if (pageNumber > totalPageNumber) {
-    redirect(`?page=${totalPageNumber}`);
-  }
 
   if (data.results.length === 0 || data.count === 0) {
     return (
--- src/app/(pages)/blog/page.tsx
+++ src/app/(pages)/blog/page.tsx
@@ -1,18 +1,17 @@
 import { Suspense } from "react";
 
 import { BlogSkeleton } from "@/components/skeletons/BlogSkeleton";
-import type { SearchParams } from "@/types/next";
 
 import { BlogServer } from "./blog.server";
 
-export default function Page({ searchParams }: { searchParams: SearchParams }) {
+export default function Page() {
   return (
     <div className="default-wrap mx-auto max-w-screen-lg !content-between">
       <h1>
         <span>Blog</span>
       </h1>
       <Suspense fallback={<BlogSkeleton />}>
-        <BlogServer searchParams={searchParams} />
+        <BlogServer />
       </Suspense>
     </div>
   );
